34f93b4ed7c0ac9baaed587a1048c678242c121f
Dante Cervantes <contacto@dantecervantes.com>
2017-01-18 20:14:33 -0600

    - Changed package for NPM PUBLISH

--- module.js
+++ module.js
@@ -1,13 +1,11 @@
-'use strict';
-
 import {SongsCollection} from 'NativeModules';
 
-var MusicFiles = {
+let MusicFiles = {
 	get : function(success,error){
 		SongsCollection.getAll((err) => {
 			error(err)
 		},(response) => {
-			var songs = response.filter((item) => {
+			let songs = response.filter((item) => {
 				//get files large than 10s
 				return item.duration > 10000
 			});
