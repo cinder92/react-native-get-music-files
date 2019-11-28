82cd7fa8400ce2aa3671fd3f5100a4d89e80f95d
Yajana N Rao <yajananrao@gmail.com>
2019-11-28 11:31:42 +0530

    Update RNAndroidStore.java

--- android/src/main/java/com/drazail/rnandroidstore/RNAndroidStore.java
+++ android/src/main/java/com/drazail/rnandroidstore/RNAndroidStore.java
@@ -38,7 +38,7 @@ public class RNAndroidStore extends ReactContextBaseJavaModule {
     private boolean getArtistFromSong = false;
     private boolean getDurationFromSong = true;
     private boolean getTitleFromSong = true;
-    private boolean getIDFromSong = false;
+    private boolean getIDFromSong = true;
     private boolean getCoversFromSongs = true;
     private String coversFolder = "/";
     private double coversResizeRatio = 1;
@@ -169,7 +169,7 @@ public class RNAndroidStore extends ReactContextBaseJavaModule {
                     item.putString("id", String.valueOf(cursor.getLong(0)));
                     item.putString("album", String.valueOf(cursor.getString(1)));
                     item.putString("author", String.valueOf(cursor.getString(2)));
-                    item.putString("cover",  "file://" +String.valueOf(cursor.getString(3)));
+                    item.putString("cover", String.valueOf(cursor.getString(3)));
                     item.putString("numberOfSongs", String.valueOf(cursor.getString(4)));
                     jsonArray.pushMap(item);
                 } while (cursor.moveToNext());
